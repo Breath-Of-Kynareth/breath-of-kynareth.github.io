129c6dd08e08f960bc5ff82dbe32c8771d3e1dd1
Joshua Hammer <jebelfor@gmail.com>
2023-10-23 00:03:16 -0500

    Starting work

---
+++ src/components/Homepage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <a href="/timestamp-maker" target="_blank"><button>Timestamp Maker</button></a>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    components: { },
+    setup() {
+
+      return {
+      }
+    }
+  }
+  </script>
+
+  <style scoped>
+</style>
