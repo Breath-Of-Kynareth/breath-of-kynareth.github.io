a41a9ef2bab4b70eb7bb47e5ba5b1fa49d87f06f
Joshua Hammer <jebelfor@gmail.com>
2023-11-03 23:55:05 -0500

    Output Examples

--- src/pages/TimestampPage.vue
+++ src/pages/TimestampPage.vue
@@ -1,23 +1,32 @@
 <template>
-  <!--<h1>Discord Timestamp Generator</h1>-->
-  <div>
-    <Dropdown filter name="timezone-select" id="timezone-select" v-model="timezone" 
-      :options=timezones placeholder="Select a Timezone" class="w-full md:w-14rem" />
-  </div>
-  <div>
-    <VueDatePicker class ="dp__theme_dark" :dark="true" v-model="date" :is-24="false" week-start="0" 
-      :timezone="timezone" :text-input="textInputOptions" @update:model-value="generateTimestamp" />
-  </div>
-  <div>
-    <div class="grid">
-      <div class="col-12" v-for="row in getRows(generatedTimestamp)" :key="row.id">
-        <div class="text-center p-3 border-round-sm bg-primary font-bold">
-          {{ row.id }}
-          <button @click="copyToClipboard(row.id)">
-            <font-awesome-icon :icon="['fas','clipboard']" flip style="color: #2f8a02" />
-          </button>
+  <Navigation />
+  <div class="container">
+    <div class="col-12 sm:col-12 selector-container">
+      <Dropdown filter name="timezone-select" id="timezone-select" v-model="timezone" 
+        :options=timezones placeholder="Select a Timezone" class="timezone-picker" />
+      <VueDatePicker class ="dp__theme_dark date-picker " :dark="true" v-model="date" :is-24="false" week-start="0" 
+        :timezone="timezone" :text-input="textInputOptions" @update:model-value="generateTimestamp" />
+    </div>
+    <div>
+      <div class="grid nested-grid">
+        <div class="col-12" v-for="row in getRows()" :key="row.id" :formatted="row.formatted">       
+          <div class="grid">
+            <div class="xl:col-5 col-12">
+              <div class="text-center p-1 border-round-sm bg-primary font-bold">
+                <button @click="copyToClipboard(row.id)" class="copy-button">
+                  <font-awesome-icon :icon="['fas','clipboard']" size="lg" style="color: #2f8a02" />
+                </button>
+                {{ row.id }}
+              </div>
+            </div>
+            <div class="xl:col-6 col-12 m-2">
+              <div class="text-center p-1 border-round-sm bg-primary font-bold m-0">
+                {{ formatedDiscordTimestamps(row.formatted) }}
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </div> 
     </div>
   </div>
 </template>
@@ -25,39 +34,32 @@
 <script lang="ts">
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
-import { ref, onMounted, watch } from 'vue';
+import { ref, onMounted } from 'vue';
 import { timeZonesNames} from "@vvo/tzdb";
 import Dropdown from 'primevue/dropdown';
-
+import Navigation from '../components/Navigation.vue';
+// @ts-ignore
+import { DateTime } from "luxon";
 
 export default {
-  components: { VueDatePicker, Dropdown },
+  components: { VueDatePicker, Dropdown, Navigation },
   setup() {
     const date = ref(new Date());
     const timezone = ref();
     const timezones = timeZonesNames;
     const generatedTimestamp = ref()
-
-
     const textInputOptions = {}
 
-    watch(generatedTimestamp.value, async () =>{
-      getRows();
-    })
-
     const getRows = () => {
-
-
-
       return [
-        { id: `<t:${generatedTimestamp.value}:d>` },
-        { id: `<t:${generatedTimestamp.value}:D>` },
-        { id: `<t:${generatedTimestamp.value}:t>` },
-        { id: `<t:${generatedTimestamp.value}:T>` },
-        { id: `<t:${generatedTimestamp.value}:f>` },
-        { id: `<t:${generatedTimestamp.value}:F>` },
-        { id: `<t:${generatedTimestamp.value}:r>` },
-        { id: `${generatedTimestamp.value}` },
+        { id: `<t:${generatedTimestamp.value}:d>`,formatted: 'd' },
+        { id: `<t:${generatedTimestamp.value}:D>`,formatted: 'D'  },
+        { id: `<t:${generatedTimestamp.value}:t>`,formatted: 't'  },
+        { id: `<t:${generatedTimestamp.value}:T>`,formatted: 'T'  },
+        { id: `<t:${generatedTimestamp.value}:f>`,formatted: 'f'  },
+        { id: `<t:${generatedTimestamp.value}:F>`,formatted: 'F'  },
+        { id: `<t:${generatedTimestamp.value}:r>`,formatted: 'R'  },
+        { id: `${generatedTimestamp.value}`, formatted: '0' },
       ]
     }
 
@@ -71,8 +73,34 @@ export default {
       navigator.clipboard.writeText(value);
     };
 
+    const formatedDiscordTimestamps = (formatChoice: string) => {
+      const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+      const dateTime = new Date(parseInt(generatedTimestamp.value) * 1000);
+      // dateTime.getHours() >= 12 ? 'PM' : 'AM'
+
+      switch(formatChoice){
+        case 'd':
+          return `${ dateTime.getMonth()+1 }/${ dateTime.getDate() }/${ dateTime.getFullYear() }`;
+        case 'D':
+          return `${ months[dateTime.getMonth()] } ${ dateTime.getDate() }, ${ dateTime.getFullYear() }`;
+        case 't':
+          return `${ dateTime.getHours() % 12 }:${ dateTime.getMinutes() } ${ dateTime.getHours() >= 12 ? 'PM' : 'AM' }`;
+        case 'T':
+          return `${ dateTime.getHours() % 12 }:${ dateTime.getMinutes() }:00 ${ dateTime.getHours() >= 12 ? 'PM' : 'AM' }`;
+        case 'f':
+          return `${ months[dateTime.getMonth()] } ${ dateTime.getDate() }, ${ dateTime.getFullYear() } ${ dateTime.getHours() % 12 }:${ dateTime.getMinutes() }`;
+        case 'F':
+          return `${ weekdays[dateTime.getDay()] }, ${ months[dateTime.getMonth()] } ${ dateTime.getDate() }, ${ dateTime.getFullYear() } ${ dateTime.getHours() % 12 }:${ dateTime.getMinutes() }`;
+        case 'R':
+          return 'In x minutes/ x Hours Ago'; // TODO: Figure out the implementation of this feature in here.
+        case '0':
+          return generatedTimestamp.value;
+      }
+    }
+
     onMounted(() => {
-      // Detect timezone and set it as default
       const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
       timezone.value = userTimezone;
       generateTimestamp();
@@ -83,17 +111,47 @@ export default {
       date,
       timezones,
       timezone,
-      generateTimestamp,
-      copyToClipboard,
       generatedTimestamp,
       textInputOptions,
-      getRows
+      generateTimestamp,
+      copyToClipboard,
+      getRows,
+      formatedDiscordTimestamps 
     }
   }
 }
 </script>
 
 <style scoped>
+  .container {
+    padding: 20px;
+  }
+
+  /* Desktop layout (1/3 of the screen horizontally) */
+  @media (min-width: 768px) {
+    .container {
+      width: 33.33%;
+      margin: auto;
+    }
+    .selector-container{
+      align-items: center;
+      display: flex;
+    }
+    .timezone-picker{
+      width: 50%;
+    }
+    .date-picker{
+      width: 50%;
+    }
+  }
+
+  /* Mobile layout (full width horizontally) */
+  @media (max-width: 767px) {
+    .container {
+      width: 100%;
+      height: auto;
+    }
+  }
 .dp__theme_dark {
     --dp-background-color: #212121;
     --dp-text-color: #fff;
