767f6342a313f1e548624e3feaf957db8cfbf7d0
Joshua Hammer <jebelfor@gmail.com>
2023-11-26 13:43:07 -0600

    Homepage additions

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -2,6 +2,10 @@
   <div>
     <Navigation />
   </div>
+  <div class='container'><img src='/BOKBanner.png'/></div>
+  <div class='container'>
+    <iframe width="1000" height="500" src="https://www.youtube.com/embed/videoseries?si=0RhCNs2aDjX2USNY&amp;list=PL-z7L6gs44NO9Nn8XzgAxH-mdLIIUXq9D" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
 </template>
   
   <script lang="ts">
@@ -17,4 +21,8 @@
 </script>
 
 <style scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+  }
 </style>
