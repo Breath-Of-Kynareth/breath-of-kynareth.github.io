200da944af231446ab52c1a50bbabb7cc8d70215
Joshua Hammer <jebelfor@gmail.com>
2023-11-26 16:12:22 -0600

    Image scaling

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -2,7 +2,7 @@
   <div>
     <Navigation />
   </div>
-  <div class='container'><img src='/BOKBanner.png'/></div>
+  <div class='container'><img src='/BOKBanner.png' class="image-container"/></div>
   <div class='container'>
     <iframe width="1000" height="500" src="https://www.youtube.com/embed/videoseries?si=0RhCNs2aDjX2USNY&amp;list=PL-z7L6gs44NO9Nn8XzgAxH-mdLIIUXq9D" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   </div>
@@ -24,5 +24,10 @@
   .container {
     display: flex;
     justify-content: center;
+  }
+  .image-container {
+    max-width: 100%;
+    height: auto;
+    display: block;
   }
 </style>
