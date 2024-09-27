a0b16d78f648ec5bf463a21b707e85fcbbb0ebb3
Joshua Hammer <jebelfor@gmail.com>
2024-09-26 23:43:13 -0500

    Video Backround

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,36 @@
 <template>
+  <div class="video-background">
+    <video autoplay loop muted playsinline class="background-video">
+      <source src="../assets/background.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  </div>
   <div id="app">
     <router-view/>
   </div>
   <notifications position="top center" max="3" />
 </template>
+
+<style scoped>
+
+.video-background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  z-index: -1;
+}
+
+video {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 100vw;
+  height: 100vh;
+  transform: translate(-50%, -50%);
+  object-fit: cover;
+}
+
+</style>
